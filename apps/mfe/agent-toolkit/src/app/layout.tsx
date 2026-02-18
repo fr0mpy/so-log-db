18b14b404aee37892d6e81eb5b3f20f3c5123849
fr0mpy <elfrompo@gmail.com>
2026-02-18 11:40:45 +0000

    feat(agent-toolkit): add ToastProvider and theme FOUC prevention
    Wrap app with ToastProvider for toast notifications. Add inline theme init script to prevent flash of unstyled content on page load.

--- apps/mfe/agent-toolkit/src/app/layout.tsx
+++ apps/mfe/agent-toolkit/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata, Viewport } from 'next'
 import { NextIntlClientProvider } from 'next-intl'
 import { getLocale, getMessages } from 'next-intl/server'
-import { ThemeProvider } from '@stackone-ui/core/providers'
+import { ThemeProvider, THEME_INIT_SCRIPT } from '@stackone-ui/core/providers'
+import { ToastProvider } from '@stackone-ui/core/toast'
 import { fontSans, fontMono } from '@stackone-ui/core/fonts/next-loader'
 import { Sidebar, SidebarProvider, MainContent, SkipLinks } from '@/components'
 import { ProviderIconPreloader } from '@/components/ProviderIcon'
@@ -44,15 +45,21 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} className={fontVariables} suppressHydrationWarning>
+      <head>
+        {/* Inline script to set theme before React hydrates (prevents FOUC) */}
+        <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
+      </head>
       <body className={fontSans.className}>
         <NextIntlClientProvider messages={messages}>
           <SkipLinks />
           <ThemeProvider>
-            <ProviderIconPreloader />
-            <SidebarProvider>
-              <Sidebar />
-              <MainContent>{children}</MainContent>
-            </SidebarProvider>
+            <ToastProvider>
+              <ProviderIconPreloader />
+              <SidebarProvider>
+                <Sidebar />
+                <MainContent>{children}</MainContent>
+              </SidebarProvider>
+            </ToastProvider>
           </ThemeProvider>
         </NextIntlClientProvider>
       </body>
